35379582f7ff521413a2e734d859752b7067f81e
Muzammil <muzammilwipro@gmail.com>
2025-06-16 13:59:48 +0530

    githubscreen added

---
+++ src/components/screens/Githubscreen.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Githubscreen = () => {
+  return (
+      <SafeAreaView>
+      <Text>Githubscreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Githubscreen
+
+const styles = StyleSheet.create({})
