752007c09db857006f442750727544a1b83893a7
Muzammil <muzammilwipro@gmail.com>
2025-06-16 20:29:14 +0530

    branch1 added

--- src/components/screens/Githubscreen.tsx
+++ src/components/screens/Githubscreen.tsx
@@ -6,7 +6,7 @@ const Githubscreen = () => {
       <SafeAreaView>
       <Text>Githubscreen</Text>
         <Text>Githubscreen2</Text>
-        <Text>branch 1 added</Text>
+        <Text>branch 1 added sucessfully</Text>
     </SafeAreaView>
   )
 }
